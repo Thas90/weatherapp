6b836c90fa0a50250c5cf7bf544a8a9c0c968f59
Thas90 <thas_idhayath@yahoo.com>
2023-11-05 09:22:07 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [ search, setSearch] = useState('')
+
+  const [ weather, setWeather] = useState({})
+
+  const APIKey="bc900deebe4b7c13448097a515026acd";
+
+  function searching(){
+    console.log("button clicked")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${APIKey}`).then(res=>res.json())
+    .then(result => setWeather(result))
+  }
+  console.log(search)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app'>
+        <div className='header'><b>My Own Weather App</b></div>
+        <div className='App-header'>
+          
+      <input type="text" placeholder="Enter your city" onChange={(e)=>setSearch(e.target.value)} />
+      <br/>
+      <button onClick={searching}>Search</button>
+      {typeof weather.main != 'undefined' ?
+      <div>
+      <h2>{weather.name}</h2>
+      <h4>{weather.main.temp}</h4>
+      <h4>Max:{weather.main.temp_max}</h4>
+
+      </div>
+      :
+      ""
+  }
+      </div>  
+      
+      </div>
+      
+      
+      
+      
   );
 }
 
